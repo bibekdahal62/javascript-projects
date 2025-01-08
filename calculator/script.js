44755bf80c5a5e8e8babcd010ac06d93d028a114
bibekdahal62 <bibekgdahal@gmail.com>
2025-01-08 20:17:33 +0545

    javascript given to calculator

---
+++ calculator/script.js
@@ -0,0 +1,87 @@
+
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll(".button");
+
+let displayValue = '';
+let operator = '';
+let firstOperand = null;
+let secondOperand = null;
+
+
+const updateDisplay = (updateValue) => {
+    display.textContent = updateValue;
+}
+
+
+const calculate = () => {
+    let result;
+
+    switch(operator){
+        case '+':
+            result = firstOperand + secondOperand;
+            break;
+        case '-':
+            result = firstOperand - secondOperand;
+            break;
+        case '*':
+            result = firstOperand * secondOperand;
+            break;
+        case '/':
+            result = firstOperand / secondOperand;
+            break;
+        default:
+            return;
+    }
+
+    updateDisplay(result);
+    displayValue = '';
+    operator = '';
+    firstOperand = result;
+    secondOperand = null;
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let value = button.textContent;
+        
+        if(!isNaN(value) || value === '.'){
+            if(value === '.' && displayValue.includes('.')){
+                return;
+            }
+            displayValue += value;
+            updateDisplay(displayValue);
+        }
+
+        if(['+', '-', '*', '?'].includes(value)){
+            if(firstOperand == null){
+                firstOperand = Number(displayValue);
+            } else if(operator){
+                secondOperand = Number(displayValue);
+                calculate();
+            }
+            operator = value;
+            displayValue = '';
+        }
+
+        if(value === '='){
+            if(firstOperand !== null && operator && displayValue !== ''){
+                secondOperand = Number(displayValue);
+                calculate();
+            }
+        }
+
+        if(value === 'C'){
+            displayValue = '';
+            operator = '';
+            firstOperand = null;
+            secondOperand = null;
+            updateDisplay(displayValue);
+        }
+
+        if(value === 'B'){
+            displayValue = displayValue.slice(0, -1);
+            updateDisplay(displayValue);
+        }
+    });
+});
